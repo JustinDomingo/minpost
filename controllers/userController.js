ccc9f4746c95569b5a8da6a8b3a9b817096c0736
JustinDomingo <88120844+JustinDomingo@users.noreply.github.com>
2021-10-15 00:26:51 -0700

    Update userController.js

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,4 +1,7 @@
 const User = require('../models/User')
+const jwt = require('jsonwebtoken')
+const dotenv = require('dotenv')
+dotenv.config()
 
 exports.register = function(req, res) {
     let user = new User(req.body)
@@ -47,5 +50,59 @@ exports.logout = function(req, res) {
 exports.showProfile = function(req, res) {
     User.findPosts(req.params.username).then((item) => {
         res.render('profile', { user: req.params.username, posts: item })
+    })
+}
+
+//--------------------------------API--------------------------------
+
+
+//Just authentication, not pushed into DB
+exports.apiRegisterJWT = function(req, res) {
+    console.log(req.body)
+    let user = new User(req.body)
+    user.apiReg().then(item => {
+        res.json(jwt.sign({ username: item.username }, process.env.SECRET, { expiresIn: '1h' }))
+    }).catch(err => {
+        let newArray = err.map((item) => {
+            return item[0]
+        })
+        res.status(500).send({ errors: newArray })
+    })
+}
+
+exports.apiLogin = function(req, res) {
+    let user = new User(req.body)
+    user.login().then(() => {
+        res.send("success")
+    }).catch(err => {
+        console.log(err)
+        res.status(500).send({ message: "Invalid Username/Password" })
+    })
+}
+
+exports.apiUsers = function(req, res) {
+    User.getUsers().then((item) => {
+        res.send(item)
+    }).catch(() => {
+        res.send("failed")
+    })
+}
+
+exports.apiUser = async function(req, res) {
+    User.getUser(req.params.username).then((user) => {
+        res.json(user)
+    }).catch(() => {
+        res.status(500).send({ error: "Not found" })
+    })
+}
+
+exports.apiRegister = function(req, res) {
+    let user = new User(req.body)
+    user.register().then(item => {
+        const ok = true
+        res.status(201).send({ jwt: jwt.sign({ item }, process.env.SECRET, { expiresIn: '1h' }), user: item, ok })
+    }).catch(err => {
+        const ok = false
+        res.status(500).send({err, ok})
     })
 }
